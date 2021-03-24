ef9f49c023711766951e5fd2046f8fc504960de8
Liubomyr <ljubik@ukr.net>
2021-03-24 21:03:34 +0200

    add Route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import "./App.css";
-import React, { Component } from "react";
+import React, { Component, lazy, Suspense } from 'react';
+import { Route } from 'react-router';
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
 import db from "./db/db.json";
 
+const HomePage = lazy(() => import('./pages/HomePage'));
+const About = lazy(() => import('./pages/About'));
 
 class App extends Component {
   state = {
     name: "",
+    location: ""
   };
 
   addContact = (el) => {
@@ -27,13 +31,25 @@ class App extends Component {
     this.setState({ [el.name]: el.value });
   };
 
+  onChangePath = (path) => {
+    this.setState({location: path});
+  };
+
   render() {
     const { addContact, getValue } = this;
     return (
       <>
       <Header />
       <Main db={db} addContact={addContact} getValue={getValue}/>
+      <Suspense>
+          <Route exact path="/" render={props => 
+            <HomePage 
+              {...props} 
+              onChangePath={this.onChangePath}
+            />}
+          />
       <Footer />
+      </Suspense>
     </>
     );
   }
---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function About () {
+
+  return (
+    <> 
+        <div> The second page</div>
+    </>
+  );
+}
+
+export default About;
